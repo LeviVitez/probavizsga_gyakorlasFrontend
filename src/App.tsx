34ef3a19eef8574a1101b1e7f2df02e7a8e5a2a1
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-05-29 21:52:41 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './App.css';
 import Main from './pages/Main';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <Main></Main>
+    <><Helmet>
+      <title> Petrik Könyvklub</title>
+    </Helmet>
+    <Main></Main></>
   );  
 }
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, FormEvent } from "react";
 
 interface Members {
     id: number;
@@ -22,15 +22,15 @@ interface State {
     created_at: string;
 }
 
-export default class Main extends Component<{},State> {  
+export default class Main extends Component<{}, State> {
 
     state: State = {
         members: [],
         name: '',
-        gender:'',
-        birth_date:'',
-        banned:false,
-        created_at:'',
+        gender: '',
+        birth_date: '',
+        banned: false,
+        created_at: '',
     }
 
     async loadMembers() {
@@ -45,34 +45,82 @@ export default class Main extends Component<{},State> {
                     members: data.members,
                 });
             };
-            }
-         catch (error) {
+        }
+        catch (error) {
             console.error('Error fetching data:', error);
         }
     }
 
-    componentDidMount() {
-        this.loadMembers();
-    }
+    addNewMember = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const { name, birth_date, gender } = this.state;
+      
+        const dbData = {
+          name: name,
+          birth_date: birth_date,
+          gender: gender
+        };
+      
+        try {
+          let response = await fetch('http://localhost:3000/api/members', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dbData),
+          });
+      
+          this.setState({
+            name: '',
+            birth_date: '',
+            gender: ''
+          });
+      
+          await this.loadMembers();
+        } catch (error) {
+          console.error('Error adding new member:', error);
+        }
+      }
+
+      async handlePayment() {
+        let response = await fetch('http://localhost:3000/api/members/1/pay', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          });
+          await this.loadMembers();
+      }
+      
+      async onLoad() {
+        await this.loadMembers();
+      }
+      
+      componentDidMount() {
+        this.onLoad();
+      }
+      
 
     pictureDecider(gender: string) {
         let genderImage;
         if (gender === 'M') {
-           return genderImage = (<img src={'male.png'} alt="" className="img-fluid" />)
-          } else if (gender === 'F') {
-            return genderImage = (<img src={'female.png'} alt="" className="img-fluid"/>)
-          } else {
-            return genderImage = (<img src={'other.png'} alt=""className="img-fluid d-block m-auto" />)
-          }
+            return genderImage = (<img src={'male.png'} alt="" className="img-fluid" />)
+        } else if (gender === 'F') {
+            return genderImage = (<img src={'female.png'} alt="" className="img-fluid" />)
+        } else {
+            return genderImage = (<img src={'other.png'} alt="" className="img-fluid d-block m-auto" />)
+        }
     }
 
     render() {
         return <><div className='ms-5 mt-5'>
-            <a href="#" className='App ms-5'>Új tag felvétele</a> <a href="https://petrik.hu/" className='App ms-3'>Petrik honlap</a>
-            <h2 className='ms-3 mt-4'>Petrik könyklub</h2>
-            <div className="col-lg-12">
+            <nav>
+                <a href="#" className='App ms-5'>Új tag felvétele</a> <a href="https://petrik.hu/" className='App ms-3'>Petrik honlap</a>
+                <h2 className='ms-3 mt-4'>Petrik könyklub</h2>
+            </nav>
+            <div className="col-lg-12 col-md-12 col-sm-12">
+                <div className="col-lg-4">
                 {this.state.members.map((members: Members) => (
-                    <div className="col-lg-4">
                         <div className="card mt-2 ms-2 me-2">
                             <div className="card-body">
                                 <h4 className="ms-2">{members.name}</h4>
@@ -80,13 +128,29 @@ export default class Main extends Component<{},State> {
                                 <p className="ms-2">Csatlakozott: &ensp; {members.created_at}</p>
                                 <div className="picture">{this.pictureDecider(members.gender)}</div>
                             </div>
+                            <button className="btn" onClick={this.handlePayment}>Tagdij befizetése</button>
                         </div>
-                    </div>
                 ))}
+                </div>
             </div>
+            <form className="d-block m-auto form text-center" onSubmit={this.addNewMember}>
+                    <h2>Tagfelvétel</h2>
+                    <div className="d-block m-auto form-control">
+                        <label htmlFor="nameInp">Név </label>
+                        <br />
+                        <input type="text" id="nameInp"  required value={this.state.name} onChange={e => this.setState({name: e.currentTarget.value})} /><br />
+                        <label htmlFor="date">Születési Idő</label>
+                        <br />
+                        <input type="date" id="date" required value={this.state.birth_date} onChange={e => this.setState({birth_date: e.currentTarget.value})}/><br />
+                        <label htmlFor="gender">Nem</label>
+                        <br />
+                        <input type="text" id="gender" required value={this.state.gender} onChange={e => this.setState({gender: e.currentTarget.value})}/><br />
+                        <button type="submit">Hozzáadás</button>
+                    </div>
+            </form>
         </div>
-        <footer className="ms-5 mt-5">
-                    <p>Készítette: Zuber Marcell</p>
+            <footer className="ms-5 mt-5">
+                <p>Készítette: Zuber Marcell</p>
             </footer></>
     }
 }
